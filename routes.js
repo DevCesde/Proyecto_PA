258538387067679fbb53521acd5d24ce046ea2b9
hervis30 <hervisjonat@gmail.com>
2023-06-09 19:53:49 -0500

    se actualiza proyecto

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
-const cors       = require('cors')
-const express    = require("express");
+const cors = require('cors')
+const express = require("express");
 const controller = require("./controllers.js");
 
 
@@ -8,29 +8,30 @@ const router = express.Router();
 
 // --------------- API REST CRUD
 
-router.get    ("/usuarios",      cors(), controller.readUsuarios);   // Read All
-router.get    ("/usuarios/:id",  cors(), controller.readUsuario);    // Read
-router.delete ("/usuarios/:id",  cors(), controller.deleteUsuario);  // Delete
-router.put    ("/usuarios/:id",  cors(), controller.updateUsuario);  // Update
-router.post   ("/usuarios",      cors(), controller.createUsuario);  // Create
+router.get("/usuarios/:usuario", cors(), controller.readUsuario);
+router.get("/usuarios", cors(), controller.readUsuarios);   // Read All
+router.get("/usuarios/:id", cors(), controller.readUsuario);    // Read
+router.delete("/usuarios/:id", cors(), controller.deleteUsuario);  // Delete
+router.put("/usuarios/:id", cors(), controller.updateUsuario);  // Update
+router.post("/usuarios", cors(), controller.createUsuario);  // Create
 
-router.get    ("/carros",     cors(), controller.readCarros);  // Read All
-router.get    ("/carros/:id", cors(), controller.readCarro);   // Read
-router.delete ("/carros/:id", cors(), controller.deleteCarro); // Delete
-router.put    ("/carros/:id", cors(), controller.updateCarro); // Update
-router.post   ("/carros",     cors(), controller.createCarro); // Create
+router.get("/carros", cors(), controller.readCarros);  // Read All
+router.get("/carros/:id", cors(), controller.readCarro);   // Read
+router.delete("/carros/:id", cors(), controller.deleteCarro); // Delete
+router.put("/carros/:id", cors(), controller.updateCarro); // Update
+router.post("/carros", cors(), controller.createCarro); // Create
 
-router.get    ("/rentas",     cors(), controller.readRentas);  // Read All
-router.get    ("/rentas/:id", cors(), controller.readRenta);   // Read
-router.delete ("/rentas/:id", cors(), controller.deleteRenta); // Delete
-router.put    ("/rentas/:id", cors(), controller.updateRenta); // Update
-router.post   ("/rentas",     cors(), controller.createRenta); // Create
+router.get("/rentas", cors(), controller.readRentas);  // Read All
+router.get("/rentas/:id", cors(), controller.readRenta);   // Read
+router.delete("/rentas/:id", cors(), controller.deleteRenta); // Delete
+router.put("/rentas/:id", cors(), controller.updateRenta); // Update
+router.post("/rentas", cors(), controller.createRenta); // Create
 
-router.get    ("/retornos",     cors(), controller.readRetornos);  // Read All
-router.get    ("/retornos/:id", cors(), controller.readRetorno);   // Read
-router.delete ("/retornos/:id", cors(), controller.deleteRetorno); // Delete
-router.put    ("/retornos/:id", cors(), controller.updateRetorno); // Update
-router.post   ("/retornos",     cors(), controller.createRetorno); // Create
+router.get("/retornos", cors(), controller.readRetornos);  // Read All
+router.get("/retornos/:id", cors(), controller.readRetorno);   // Read
+router.delete("/retornos/:id", cors(), controller.deleteRetorno); // Delete
+router.put("/retornos/:id", cors(), controller.updateRetorno); // Update
+router.post("/retornos", cors(), controller.createRetorno); // Create
 
 
 
