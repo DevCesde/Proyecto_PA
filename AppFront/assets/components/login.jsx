23280b52d9d09df504a166bb390ac6010b47b863
Jhonatan Rojas Bastidas <jonatanrojasbastidas@gmail.com>
2023-06-11 18:50:37 -0500

    wilmer cambios en el front

--- AppFront/assets/components/login.jsx
+++ AppFront/assets/components/login.jsx
@@ -1,139 +1,158 @@
-import {Button,TextInput} from 'react-native-paper'
+import { Button, TextInput } from 'react-native-paper'
 import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import useRegistroVehiculos from '../hooks/useRegistroVehiculos';
+
 import { useForm, Controller } from "react-hook-form";
 import React from "react";
 import axios from 'axios';
 
 
 
-export default function Login(navigation){
-    
-  const [errormessage, setErrorMessage] = useState('');
-  const [message, setMessage] = useState(''); 
-   
+export default function Login({ navigation }) {
 
-const { control, handleSubmit, formState: { errors } } = useForm({
-    defaultValues: {
-    usuario: '',
-    contrasena:''
-    }
-  });
+    const { user, setUser } = useRegistroVehiculos();
 
-  const onSubmit = data => console.log(data)
-
-  
-  const onSearch = async(data) => {
+    const [errormessage, setErrorMessage] = useState('');
+    const [message, setMessage] = useState('');
 
 
-    try { const response = await axios.get(`http://127.0.0.1:3000/api/usuarios/${data.usuario}`); console.log(response.data)
-    if (response.data.usuario === data.usuario && response.data.contrasena === data.contrasena){ // Encuentra el usuario 
-     console.log("conectado")
-     if(response.data.role === "1"){
-        console.log("admin")
-     }
-     else{
-      console.log("user")
-     }
-      
-      setErrorMessage(false); 
-      setMessage(''); } 
-    else{ 
-      setErrorMessage(true); 
-      setMessage("Usuario no encontrado",
-        console.log("Usuario no encontrado")); 
-      setTimeout(()=>{ 
-        setMessage(''); 
-      },2000) 
-      
-    } } catch (error) { console.log(error) 
-      alert(error) } 
-    finally{ 
-      //setLoading(false); 
-    } };
-  
+    const { control, handleSubmit, formState: { errors }, reset } = useForm({
+        defaultValues: {
+            usuario: '',
+            contrasena: ''
+        }
+    });
 
-    return(
-    <View style={styles.container} >
-    <Text style={{ marginBottom: 10,fontSize:25,fontWeight:'bold' }}>Inicio de Sesión</Text>
-    <Text style={{color:'red'}}>{setErrorMessage}</Text>
-    <Text style={{color:'gray'}}>{setMessage} </Text>
+    const onSubmit = data => console.log(data)
 
 
+    const onSearch = async (data) => {
 
 
-           {/*usuario */}
-      <Controller
-      control={control}
-      rules={{
-       required: true,
-       pattern:/^[A-Za-zÁÉÍÓÚáéíóúñÑ0-9]+$/g
-      }}
-      render={({ field: { onChange, onBlur, value } }) => (
-        <TextInput
-          placeholder="Username"
-          onBlur={onBlur}
-          onChangeText={onChange}
-          value={value}
-          style={{marginTop:10}}
-          left={<TextInput.Icon icon="account" />}
-          
-        />
-      )}
-      name="usuario"
-    />
-    {errors.usuario?.type==='required' && <Text>Este Campo es Obligatorio</Text>}
-    {errors.usuario?.type ==='pattern' && <Text>Escriba un Nombre solo con Letras y Espacios</Text>}
+        try {
+            const response = await axios.get(`http://192.168.1.2:3000/api/usuarios/${data.usuario}`); console.log(response.data)
+            if (response.data.usuario === data.usuario && response.data.contrasena === data.contrasena) { // Encuentra el usuario 
+                console.log("conectado")
+                if (response.data.role === "1") {
+                    //navigation.navigate(/*devolucion de vehiculos o lista de vehiculos*/)
+
+                    console.log("User");
+                    setUser(false);
+                    reset();
+                    navigation.navigate('vehiculoDisponible');
+                }
+                else if (response.data.role === "2") {
+                    //navigation.navigate(/* renta de vehiculo*/)
+                    console.log("Admin");
+                    setUser(true);
+                    reset();
+                    navigation.navigate('vehiculoDisponible');
+                }
+
+                setErrorMessage(false);
+                setMessage('');
+            }
+            else {
+                setErrorMessage(true);
+                setMessage("Usuario no encontrado",
+                    console.log("Usuario no encontrado"));
+                setTimeout(() => {
+                    setMessage('');
+                }, 2000)
+
+            }
+        } catch (error) {
+            console.log(error)
+            alert(error)
+        }
+        finally {
+            //setLoading(false); 
+        }
+    };
 
 
-    
-      {/*contrasena */}
-      <Controller
-      control={control}
-      rules={{
-       required: true,
-       pattern:/(?=.*\d)(?=.*[A-Za-zÁÉÍÓÚáéíóúñÑ])[A-Za-zÁÉÍÓÚáéíóúñÑ0-9]+/g
-      }}
-      render={({ field: { onChange, onBlur, value } }) => (
-        <TextInput
-          placeholder="Contraseña"
-          onBlur={onBlur}
-          onChangeText={onChange}
-          value={value}
-          secureTextEntry
-          style={{marginTop:10}}
-          left={<TextInput.Icon icon="key" />}
-        />
-      )}
-      name="contrasena"
-    />
-    {errors.contrasena?.type==="required" && <Text>Este Campo es Obligatorio</Text>}
-    {errors.contrasena?.type==="pattern" && <Text>El Password Debe contener  números y letras</Text>}
+    return (
+        <View style={styles.container} >
+            <Text style={{ marginBottom: 10, fontSize: 25, fontWeight: 'bold' }}>Inicio de Sesión</Text>
+            <Text style={{ color: 'red' }}>{setErrorMessage}</Text>
+            <Text style={{ color: 'gray' }}>{setMessage} </Text>
 
 
 
-    <Button 
-      icon="door" 
-      mode="contained" 
-      buttonColor='#B3AE4F'
-      textColor='white'
-      onPress={handleSubmit(onSearch)}
-      style={{marginTop:10,width:250}}
-      >
-      Ingresar
-    </Button>
 
-    <Button style={{marginTop:10,width:250}} onPress={()=>{navigation.navigate('Registrar')}} buttonColor='#66654B' textColor='white' icon="car-arrow-right">Registrar</Button>
-    <Button style={{marginTop:10,width:250}} onPress={()=>{navigation.navigate('ResetPassword')}} buttonColor='#66654B' textColor='white' icon="help">Recuperar Contraseña</Button>
-  </View>
+            {/*usuario */}
+            <Controller
+                control={control}
+                rules={{
+                    required: true,
+                    pattern: /^[A-Za-zÁÉÍÓÚáéíóúñÑ0-9]+$/g
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        placeholder="Username"
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                        style={{ marginTop: 10 }}
+                        left={<TextInput.Icon icon="account" />}
+
+                    />
+                )}
+                name="usuario"
+            />
+            {errors.usuario?.type === 'required' && <Text>Este Campo es Obligatorio</Text>}
+            {errors.usuario?.type === 'pattern' && <Text>Escriba un Nombre solo con Letras y Espacios</Text>}
+
+
+
+            {/*contrasena */}
+            <Controller
+                control={control}
+                rules={{
+                    required: true,
+                    pattern: /(?=.*\d)(?=.*[A-Za-zÁÉÍÓÚáéíóúñÑ])[A-Za-zÁÉÍÓÚáéíóúñÑ0-9]+/g
+                }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        placeholder="Contraseña"
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                        secureTextEntry
+                        style={{ marginTop: 10 }}
+                        left={<TextInput.Icon icon="key" />}
+                    />
+                )}
+                name="contrasena"
+            />
+            {errors.contrasena?.type === "required" && <Text>Este Campo es Obligatorio</Text>}
+            {errors.contrasena?.type === "pattern" && <Text>El Password Debe contener  números y letras</Text>}
+
+
+
+            <Button
+                icon="door"
+                mode="contained"
+                buttonColor='#B3AE4F'
+                textColor='white'
+                onPress={handleSubmit(onSearch)}
+                style={{ marginTop: 10, width: 250 }}
+            >
+                Ingresar
+            </Button>
+
+            <Button style={{ marginTop: 10, width: 250 }} onPress={() => { navigation.navigate('registroUsuario') }} buttonColor='#66654B' textColor='white' icon="car-arrow-right">Registrar</Button>
+            <Button style={{ marginTop: 10, width: 250 }} onPress={() => { navigation.navigate('ResetPassword') }} buttonColor='#66654B' textColor='white' icon="help">Recuperar Contraseña</Button>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
